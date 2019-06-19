2e73ff9002cd3ad75a9ba028bb1c476641302b2f
Diego dos Santos <diegodossantos.es@gmail.com>
2019-06-19 03:02:43 +0200

    Updates

---
+++ precache-manifest.af7857df2caed8327a809895a7fd2634.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee6bb3f32237961c3f6aada3977ed7e",
+    "url": "/NewPersonalApp-ReactJS/index.html"
+  },
+  {
+    "revision": "59bbc1ad7f6099fef06a",
+    "url": "/NewPersonalApp-ReactJS/static/css/main.2f1eecf4.chunk.css"
+  },
+  {
+    "revision": "3a5ea085dd9a2f9123c1",
+    "url": "/NewPersonalApp-ReactJS/static/js/2.5d755fd1.chunk.js"
+  },
+  {
+    "revision": "59bbc1ad7f6099fef06a",
+    "url": "/NewPersonalApp-ReactJS/static/js/main.f93b7190.chunk.js"
+  },
+  {
+    "revision": "f22fdd091eaae7f52699",
+    "url": "/NewPersonalApp-ReactJS/static/js/runtime~main.b9225468.js"
+  }
+]);
